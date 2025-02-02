22b6e84da05a785c64a8f909898c536322731e16
pbairu03 <bairupa@mail.uc.edu>
2025-02-02 12:53:57 -0500

    Update index.js

--- api/HTTPtrigger/index.js
+++ api/HTTPtrigger/index.js
@@ -1,17 +1,28 @@
-const { ConnectionPool } = require('mssql');
+const sql = require('mssql');
+
 module.exports = async function (context, req) {
-const config = { user: process.env.SQL_USER, password:
-process.env.SQL_PASSWORD, server: process.env.SQL_SERVER, database:
-process.env.SQL_DATABASE, options: { encrypt: true }};
-const pool = new ConnectionPool(config);
-try {
-await pool.connect();
-const result = await pool.request().query('SELECT Country, COUNT(*)
-AS StudentCount FROM Students GROUP BY Country');
-context.res = { status: 200, body: result.recordset };
-} catch (err) {
-context.res = { status: 500, body: err.message };
-} finally {
-pool.close();
-}
+    const config = {
+        user: process.env.SQL_USER,
+        password: process.env.SQL_PASSWORD,
+        server: process.env.SQL_SERVER,
+        database: process.env.SQL_DATABASE,
+        options: { encrypt: true }
+    };
+
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request().query('SELECT Country, COUNT(*) AS StudentCount FROM Students GROUP BY Country');
+
+        context.res = {
+            status: 200,
+            body: result.recordset
+        };
+    } catch (err) {
+        context.res = {
+            status: 500,
+            body: err.message
+        };
+    } finally {
+        sql.close();
+    }
 };
